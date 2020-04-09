df98b3dec099f31778d653373bca7507e253778c
Emiliano Galeana <galeanaara@ciencias.unam.mx>
2020-04-08 23:13:30 -0500

    comentarios

--- Tareas/Tarea4/patitos.hs
+++ Tareas/Tarea4/patitos.hs
@@ -15,13 +15,6 @@ import Data.Char
 
 type Generation = [[String]]
 
-
--- format :: Show a => [a] -> String
--- format = concat . zipWith (++) (cycle [" | "]) . map show
-
--- | printArray. Imprime la matriz 'bonito'
-printArray arr = mapM_ (putStrLn . unwords) $ map (map show) $ chunksOf 1 arr
-
 -- | genZero. Construye una generación inicial a partir de una cadena de entrada.
 genZero :: String -> Generation
 genZero xs =
@@ -35,39 +28,48 @@ genZero xs =
     then tblr
     else error "Pusiste un mal input \129414\128299"
 
+-- | evolution. Función que dada una generación, nos regresa la siguiente.
 evolution :: Generation -> Generation
 evolution [] = []
 evolution g@(x:xss) =
   let
     neigh = sort $ neighbours (multZ (zipper g 1 (length x)) 0)
   in
-    retGen (map (\x -> snd x) (duckMutation (aplana neigh []) [])) (length g) []
+    retGen (map (\x -> snd x) (duckMutation (aplana neigh []) [])) (length g)
 
+-- | generations. Función que dada una generación, nos regresa una lista de las
+--                generaciones siguientes hasta que ya no se puedan generar.
 generations :: Generation -> [Generation]
 generations [] = error "\129414\129414\129414\129414, no hay suficientes \129414"
 generations e = genAux e [e]
+  where genAux e gens =
+          let
+            e' = evolution e
+          in
+            if (e' == e)
+            then gens
+            else genAux e' (gens++[e'])
 
+---------------------------------------------------------------------------------
+--------                          AUXILIARES                             --------
+---------------------------------------------------------------------------------
 
--- genAux [] gens = gens
-genAux e gens =
-  let
-    e' = evolution e
-  in
-    if (e' == e)
-    then gens
-    else genAux e' (gens++[e'])
+-- | printArray. Imprime la matriz 'bonito'
+printArray arr = mapM_ (putStrLn . unwords) $ map (map show) $ chunksOf 1 arr
 
+-- | retGen. Función que toma una lista, y regresa listas de listas de tamaño n.
+retGen [] n = []
+retGen l@(x:y:z:xs) n = [take n l]++retGen xs n
 
-
-retGen [] n ac = ac
-retGen l@(x:y:z:xs) n ac = retGen xs n (ac++[take n l])
-
+-- | duckMutation. Función que aplica las mutaciones necesarias a la siguiente
+--                 generación. Se basa en las siguientes reglas:
 -- Un pato bueno se mantiene bueno si es adyacente a exactamente dos o tres
 -- patos también buenos, en otro caso se transforma en un pato malo.
 
 -- Si un pato malo es adyacente a exactamente tres patos buenos entonces se
 -- convierte en bueno, en otro caso sigue siendo malo.
-
+-- |NOTA| La i-esima casilla de la siguiente generación va a estar dada por los
+-- vecinos de la i-esima casilla en la generación actual.
 duckMutation [] mut = mut
 duckMutation ((c,ad):xs) mut = if (snd c) == "G"
                                then if (snd ad == 2 || snd ad == 3)
@@ -77,27 +79,34 @@ duckMutation ((c,ad):xs) mut = if (snd c) == "G"
                                     then duckMutation xs (mut++[(fst c,"G")])
                                     else duckMutation xs (mut++[(fst c,"B")])
 
-
+-- | aplana. Función que toma todos los vecinos de una casilla para todas las
+--           casillas. Y regresa la casilla con el número de vecinos buenos y
+--           malos.
 aplana [] ac = ac
 aplana ((a,n):xs) ac = if snd n == "B"
                        then aplana xs (agrega (a, (1,0)) ac)
                        else aplana xs (agrega (a, (0,1)) ac)
 
+-- | agrega. Función dada una lista de tuplas, agrega una nueva si no existe, y
+--           si existe, suma a los vecinos que tiene.
 agrega (a,s) [] = [(a,s)]
 agrega (a,s) (x:xs) = if a == fst x
                       then [(a, sumT s (snd x) )] ++ xs
                       else [x] ++ agrega (a,s) xs
+  where sumT (a,b) (c,d) = (a+c, b+d)
 
-sumT (a,b) (c,d) = (a+c, b+d)
-
+-- | zipper. Función que le asigna las mismas coordenadas a las posiciones de
+--           las listas.
 zipper [] _ _ = []
 zipper (x:xss) n m = [zip [n..m] x] ++ zipper xss (n) (m)
 
+-- | multZ. Función que le aplica una 'profundidad' a las coordenadas.
 multZ [] _ = []
 multZ (x:xss) n = [map (\y -> (fst y+n, snd y)) x] ++ multZ xss (n+length x)
+
+
 -- https://stackoverflow.com/questions/11267637/neighbours-in-2dm-matrix-represented-as-list-of-list?fbclid=IwAR1oGKfuSO63LHGaYSGWIDhNrbU4wC2qJh_HlYOhgxm0phPBo9iKyiEd_0k
 
--- podemos usar esto con tuplas para poder identificarlos
 rightList (x:y:rest) = (x,y) : rightList (y:rest)
 rightList _ = []
 
