ea9660fed388c1cede3411f7fc80dcae0728a7d7
Emiliano Galeana <galeanaara@ciencias.unam.mx>
2020-04-21 23:26:54 -0500

    triangulares sin la firma

--- Tareas/Recuperacion/programacionOrigami.hs
+++ Tareas/Recuperacion/programacionOrigami.hs
@@ -15,11 +15,14 @@ module ProgramacionOrigami where
 -- número.
 factorion :: Int -> Int
 factorion i = foldr (\x y -> fib x + y) 0 (mapF (\x -> read [x] :: Int) (show i))
+  where fib 1 = 1
+        fib n = n*fib(n-1)
 
 -- | iflip. Función que toma un número natural n y regresa el número construido
 --          con los dígitos en orden inverso.
 iflip :: Int -> Int
-iflip = error ""
+iflip i = foldl toDigit 0 $ reverseF $ mapF (\x -> read [x] :: Int) (show i)
+  where toDigit n d = 10*n + d
 
 -- | binarios. Función que recibe una lista de números y regresa una lista de
 --             números binarios asociados a los números orginales.
@@ -30,17 +33,21 @@ binarios = error ""
 --                 que contiene únicamente aquellos que son triangulares.
 -- Un número triangular es aquel que puede recomponerse en la forma de un
 -- tríangulo equilátero.
-triangulares :: [Int] -> [Int]
-triangulares = error ""
+-- triangulares :: [Int] -> [Int]
+triangulares l = mapF (\x -> round x) $ filterF tP l
 
 
 
+t n = div (n*(n+1)) 2
 
-fib 1 = 1
-fib n = n*fib(n-1)
+tP n = (\x -> x == fromInteger (round x)) (sqrt(8*n + 1))
 
 mapF f [] = []
 mapF f xs   = foldr (\y ys -> (f y):ys) [] xs
 
 reverseF :: [a] -> [a]
 reverseF xs = foldr (\b g x -> g (b : x)) id xs []
+
+filterF :: (a -> Bool) -> [a] -> [a]
+filterF _ [] = []
+filterF p xs = foldr (\x xs -> if (p x) then x:xs else xs) [] xs
