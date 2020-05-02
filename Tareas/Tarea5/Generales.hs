354067c4099cf627b7e435e73b741e7012d033a7
Emiliano Galeana <galeanaara@ciencias.unam.mx>
2020-05-02 18:16:19 -0500

    depth con un auxiliar. foldg con otra firma. CHECAR

--- Tareas/Tarea5/Generales.hs
+++ Tareas/Tarea5/Generales.hs
@@ -11,7 +11,7 @@ module Generales where
 
 import Data.List
 
-data Gtree a = Void | Node a [Gtree a] deriving (Show)
+data Gtree a = Node a [Gtree a] deriving (Show)
 -- agregar Void para tener el árbol vacío, sino tenemos un árbol de al menos un
 -- elemento
 
@@ -19,13 +19,26 @@ data BT a = VoidBT | NodeBT (BT a) a (BT a) deriving (Show)
 
 -- | size. Función que calcula el tamaño (número de elementos) de un árbol
 size :: Gtree a -> Int
-size Void            = 0
 size (Node n [])     = 1
 size (Node n (x:xs)) = size x + size (Node n xs)
 
 -- | depth. Función que calcula la profundidad (número de niveles) de un árbol.
 depth :: Gtree a -> Int
-depth = error ""
+depth t = depthAux t 0
+
+depthAux (Node n []) m = 1 + m
+depthAux (Node n [x]) m =
+  let
+    m' = depthAux x 0
+  in
+    1 + max m m'
+depthAux (Node n (x:y:ys)) m =
+  let
+    mx = depthAux x 0
+    my = depthAux y 0
+  in
+    depthAux (Node n ys) (max (max mx my) m)
+
 
 -- | tran. Función que transforma un árbol general en uno binario. No es
 --         necesario que esté ordenado.
@@ -34,35 +47,37 @@ tran = listToBin . genToList
 
 -- | mapg. Función de orden superior que está basada en map para listas.
 mapg :: (a -> b) -> Gtree a -> Gtree b
-mapg f (Void)      = Void
 mapg f (Node n []) = Node (f n) []
 mapg f (Node n xs) = Node (f n) (map (mapg f) xs)
 
 -- | foldg. Función de orden superior que está basada en fold para listas.
-foldg :: (a -> [b] -> b) -> Gtree a -> b
-foldg = error ""
+-- foldg :: (a -> [b] -> b) -> Gtree a -> b
+foldg :: (a -> b -> b) -> b -> Gtree a -> b
+foldg f e t =
+  let
+    lt = genToList t
+  in
+    foldr f e lt
 
 -- | searchg. Función que verifica si un elemento pertenece al árbol general.
 searchg :: (Eq a) => a -> Gtree a -> Bool
-searchg e (Void)          = False
 searchg e (Node n [])     = n == e
 searchg e (Node n (x:xs)) = searchg e x || searchg e (Node n xs)
 
 -- | genToList. Función que pasa un Gtree a una lista (lo aplana).
 genToList :: Gtree a -> [a]
-genToList Void            = []
 genToList (Node n [])     = [n]
 genToList (Node n (x:xs)) = genToList x ++ genToList (Node n xs)
 
 -- | listToBin. Función que pasa una lista a un árbol binario de tipo 1.
 listToBin :: [a] -> BT a
-listToBin [] = VoidBT
 listToBin l  =
   let
     (a,b) = splitAt (div (length l) 2) l
   in
     NodeBT (listToBin a) (b !! 0) (listToBin (drop 1 b))
 
-t3 = (Node 3 [Void])
+t3 = (Node 3 [])
 t2 = (Node 3 [Node 2 [], Node 1 []])
 t1 = (Node 3 [Node 2 [Node 3 [Node 1 [], Node 2 []]], Node 1 []])
+t4 = (Node 1 [Node 2 [Node 3 []]])
