25b59097cd6f20d8bd7c33c01aa9e63bf2903885
Emiliano Galeana <galeanaara@ciencias.unam.mx>
2020-06-16 01:19:17 -0500

    reader

---
+++ Tareas/Proyecto/Main.hs
@@ -0,0 +1,29 @@
+import System.Environment
+import Text.Printf
+
+f :: Eq a => a -> [a] -> Bool
+f cmt = \x -> x !! 0 /= cmt
+
+parseFile :: FilePath -> IO (String, [String])
+parseFile file =
+  do
+    chords <- readFile file
+    let (a,b) = (\(x:xs) -> (x,xs)) $ cleanComment (f '#') $ lines chords
+    return (a, b)
+
+cleanComment :: (String -> Bool) -> [String] -> [String]
+cleanComment p []     = []
+cleanComment p (x:xs) = if p x
+                        then [x] ++ cleanComment p xs
+                        else cleanComment p xs
+
+main = do
+  args <- getArgs
+  case args of
+    [file] -> do
+      (t, c) <- parseFile file
+      printf "YEIH" -- TODO: EJECUTAR EL PROGRAMA
+    _ -> printf "Error. No hay argumentos.\n"
+      
+                          
+
