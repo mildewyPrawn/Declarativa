0688e767d0b3b92dba7bf7da536c2eb0c1182ea2
Emiliano Galeana <galeanaara@ciencias.unam.mx>
2020-06-16 01:31:08 -0500

    delete comments and concat chords

--- Tareas/Proyecto/Main.hs
+++ Tareas/Proyecto/Main.hs
@@ -4,18 +4,21 @@ import Text.Printf
 f :: Eq a => a -> [a] -> Bool
 f cmt = \x -> x !! 0 /= cmt
 
-parseFile :: FilePath -> IO (String, [String])
+parseFile :: FilePath -> IO (String, String)
 parseFile file =
   do
     chords <- readFile file
     let (a,b) = (\(x:xs) -> (x,xs)) $ cleanComment (f '#') $ lines chords
-    return (a, b)
+    return (a, concater b)
 
 cleanComment :: (String -> Bool) -> [String] -> [String]
 cleanComment p []     = []
 cleanComment p (x:xs) = if p x
                         then [x] ++ cleanComment p xs
                         else cleanComment p xs
+
+concater :: [String] -> String
+concater = foldr (\x -> (\y -> x ++ " " ++ y)) []
 
 main = do
   args <- getArgs
