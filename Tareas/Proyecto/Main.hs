20c67f26c9a8a1a8eaee7c42634224d8448631c8
Emiliano Galeana <galeanaara@ciencias.unam.mx>
2020-06-16 21:18:44 -0500

    import Proyecto

--- Tareas/Proyecto/Main.hs
+++ Tareas/Proyecto/Main.hs
@@ -1,5 +1,6 @@
 import System.Environment
 import Text.Printf
+import Proyecto
 
 f :: Eq a => a -> [a] -> Bool
 f cmt = \x -> x !! 0 /= cmt
@@ -18,6 +19,8 @@ cleanComment p (x:xs) = if p x
                         else cleanComment p xs
 
 concater :: [String] -> String
+-- TODO: en vez de " " concatenar "S" que significa silencio. i.e un silencio
+-- cada saltao de línea
 concater = foldr (\x -> (\y -> x ++ " " ++ y)) []
 
 main = do
@@ -25,8 +28,12 @@ main = do
   case args of
     [file] -> do
       (t, c) <- parseFile file
-      printf "YEIH" -- TODO: EJECUTAR EL PROGRAMA
+      let new_c = translate c
+      print new_c
+      printf "\n" -- TODO: EJECUTAR EL PROGRAMA
     _ -> printf "Error. No hay argumentos.\n"
-      
-                          
 
+interactive file = do
+  (t, c) <- parseFile file
+  return $ translate c
+  -- printf "YEIH\n" -- TODO: EJECUTAR EL PROGRAMA
