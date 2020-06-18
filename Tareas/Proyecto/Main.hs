a7d24f1d4cc684024e04527318dd1beee27f7a71
Emiliano Galeana <galeanaara@ciencias.unam.mx>
2020-06-18 17:49:30 -0500

    call Music

--- Tareas/Proyecto/Main.hs
+++ Tareas/Proyecto/Main.hs
@@ -1,39 +1,49 @@
 import System.Environment
 import Text.Printf
 import Proyecto
+import Music
 
+-- | f. Función que valida si es o no un comentario.
+--      Está aparte por si queremos cambiar la validación
 f :: Eq a => a -> [a] -> Bool
 f cmt = \x -> x !! 0 /= cmt
 
-parseFile :: FilePath -> IO (String, String)
+-- | parseFile. Función que lee el archivo, y nos separa el tiempo y los acordes.
+parseFile :: FilePath -> IO (Float, String)
 parseFile file =
   do
     chords <- readFile file
     let (a,b) = (\(x:xs) -> (x,xs)) $ cleanComment (f '#') $ lines chords
-    return (a, concater b)
+    return (read a :: Float, concater b)
 
+-- | cleanComment. Función que quita los comentarios.
 cleanComment :: (String -> Bool) -> [String] -> [String]
 cleanComment p []     = []
 cleanComment p (x:xs) = if p x
                         then [x] ++ cleanComment p xs
                         else cleanComment p xs
 
-concater :: [String] -> String
+-- | concater. Función que concatena las líneas de los acordes del archivo.
 -- TODO: en vez de " " concatenar "S" que significa silencio. i.e un silencio
 -- cada saltao de línea
+concater :: [String] -> String
 concater = foldr (\x -> (\y -> x ++ " " ++ y)) []
 
+-- | MAIN.
 main = do
   args <- getArgs
   case args of
     [file] -> do
       (t, c) <- parseFile file
       let new_c = translate c
-      print new_c
-      printf "\n" -- TODO: EJECUTAR EL PROGRAMA
+      _ <- play 7 new_c
+      return ()
     _ -> printf "Error. No hay argumentos.\n"
 
+-- | parte interactiva del main.
 interactive file = do
   (t, c) <- parseFile file
-  return $ translate c
+  _ <- play t $ translate c
+  return "Ta Tan"
+  -- return $ translate c
   -- printf "YEIH\n" -- TODO: EJECUTAR EL PROGRAMA
